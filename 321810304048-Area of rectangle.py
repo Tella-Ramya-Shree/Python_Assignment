a7e8b1aa1b844b11571dc7fad0c2370231f45e63
Tella Ramya Shree <67000854+Tella-Ramya-Shree@users.noreply.github.com>
2020-06-18 16:26:09 +0530

    Add files via upload

---
+++ 321810304048-Area of rectangle.py
@@ -0,0 +1,5 @@
+#Area of rectangle
+width= float(input('Enter the width of the rectangle:'))
+height=float(input('Enter the height of the rectangle:'))
+Area= width * height
+print('Area of the rectangle:', Area)
